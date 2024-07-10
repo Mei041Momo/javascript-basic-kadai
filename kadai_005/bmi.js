fc9e38c5c23f97423e109ce8c3acdd40ec47b711
Mei Osaki <163366726+Mei041Momo@users.noreply.github.com>
2024-07-10 14:18:51 +0900

    Add kadai_005

---
+++ kadai_005/bmi.js
@@ -0,0 +1,11 @@
+//体重の宣言と代入同時
+let weight = 68;
+console.log(weight);
+
+//身長の宣言と代入同時
+let height = 1.7;
+console.log(height);
+
+//bmiの宣言と代入
+let bmi = weight / (height * height);
+console.log(bmi);
