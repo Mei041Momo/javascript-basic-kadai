f5a34c4056c07275e000923d14d9a2749045d599
Mei Osaki <163366726+Mei041Momo@users.noreply.github.com>
2024-07-19 11:37:05 +0900

    Add mdn.js

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,11 @@
+
+
+
+const date = new Date("2023/8/22");
+const [year, month, day] = [
+    date.getFullYear(),
+    date.getMonth() + 1,
+    date.getDate(),
+];
+
+console.log(year + '年' + month + '月' + day + '日');
